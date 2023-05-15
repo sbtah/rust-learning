234f81e8a6e9c3aae80ff4455bf570c83fe6e1e3
Sbtah <shisastore@protonmail.com>
2023-05-15 21:43:09 +0200

    Cargo package manager.

---
+++ hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/main/2-Learning/Rust/FOSS-Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/main/2-Learning/Rust/FOSS-Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/main/2-Learning/Rust/FOSS-Rust/hello_cargo/target/debug/hello_cargo: /home/main/2-Learning/Rust/FOSS-Rust/hello_cargo/src/main.rs
