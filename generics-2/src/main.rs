519db525a4f9778527cf7bc90cde55ef12009c63
Grzegorz.Z <grzegorz.zy@protonmail.com>
2026-03-25 09:06:06 +0100

    Save progress.

---
+++ generics-2/src/main.rs
@@ -0,0 +1,37 @@
+struct TreasureChest<T> {
+    captain: String,
+    treasure: T,
+}
+
+impl<T> TreasureChest<T> {
+    fn capital_captain(&self) -> String {
+        self.captain.to_uppercase()
+    }
+}
+
+
+enum Cheesesteak<T> {
+    
+}
+
+
+fn main() {
+    let gold_chest = TreasureChest {captain: String::from("boe"), treasure: "elo"};
+    let silver_chest = TreasureChest {captain: String::from("Joel"), treasure: 22};
+}
+
+
+// fn identity(value: i32) -> i32 {
+//     value
+// }
+
+
+fn identity<T>(value: T) -> T {
+    value
+}
+
+
+// Many Generics.
+fn make_tuple<T, U>(first: T, second: U) -> (T, U) {
+    (first, second)
+}
