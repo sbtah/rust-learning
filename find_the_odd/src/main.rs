e4730237d6a04f9cecfcc48ab92d0e16dc72b902
Grzegorz.Z <grzegorz.zy@protonmail.com>
2026-04-19 18:48:04 +0200

    Save learning progress.

--- find_the_odd/src/main.rs
+++ find_the_odd/src/main.rs
@@ -1,9 +1,26 @@
+/// Given an array of integers, find the one that appears an odd number of times.
+
+/// There will always be only one integer that appears an odd number of times.
+/// Examples
+
+/// [7] should return 7, because it occurs 1 time (which is odd).
+/// [0] should return 0, because it occurs 1 time (which is odd).
+/// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+/// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+/// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+// /// 
+/// 
+/// 
+/// 
+/// 
+
 use std::collections::HashMap;
 
 
 fn main() {
     let test_arr = [1,2,2,3,3,3,4,3,3,3,2,2,1];
-    find_odd(&test_arr);
+    let res = find_odd(&test_arr);
+    println!("res, {}", res);
 }
 
 
@@ -14,6 +31,11 @@ fn find_odd(arr: &[i32]) -> i32 {
         let count = results.entry(*num).or_insert(0);
         *count += 1;
     }
-    println!("Results: {:#?}", results);
-    1
+
+    for (key, value) in results {
+        if value % 2 == 1 {
+            return key
+        }
+    }
+    0
 }
